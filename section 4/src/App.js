2cda219f5d195ebbd6d3360da5d960cec916eb0c
Rafael Guerra <rafaelsguerra@hotmail.com>
2018-07-04 18:17:40 -0300

    alterando state corretamente

--- section 4/src/App.js
+++ section 4/src/App.js
@@ -13,9 +13,10 @@ class App extends Component {
   }
 
   deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons;
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];  
     persons.splice(personIndex, 1);
-    this.setState({persons: persons})
+    this.setState({persons: persons});
   }
 
   nameChangeHandler = (event) => {
